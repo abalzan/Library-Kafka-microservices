aaacf055d4382898aee2f940f2795eb21ac53539
Andrei Balzan <andreibalzan@gmail.com>
2021-09-23 15:45:25 +0100

    producing events in a synchronous way

--- library-event-producer/src/main/java/com/andrei/libraryeventproducer/controller/LibraryEventsController.java
+++ library-event-producer/src/main/java/com/andrei/libraryeventproducer/controller/LibraryEventsController.java
@@ -4,25 +4,32 @@ import com.andrei.libraryeventproducer.domain.LibraryEvent;
 import com.andrei.libraryeventproducer.producer.LibraryEventProducer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 @RestController
 @RequestMapping("/v1/library-event")
 @RequiredArgsConstructor
+@Slf4j
 public class LibraryEventsController {
 
     private final LibraryEventProducer libraryEventProducer;
 
     @PostMapping()
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent event) throws JsonProcessingException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent event) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
 
-        libraryEventProducer.sendLibraryEvent(event);
-
+//        libraryEventProducer.sendLibraryEvent(event);
+        final SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(event);
+        log.info("sendResult is {}", sendResult.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(event);
     }
 }
--- library-event-producer/src/main/java/com/andrei/libraryeventproducer/producer/LibraryEventProducer.java
+++ library-event-producer/src/main/java/com/andrei/libraryeventproducer/producer/LibraryEventProducer.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Component
 @Slf4j
 @RequiredArgsConstructor
@@ -36,6 +40,22 @@ public class LibraryEventProducer {
                 handleSuccess(key, value, result);
             }
         });
+    }
+
+    public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+        SendResult<Integer, String> sendResult = null;
+
+
+        try {
+            sendResult = kafkaTemplate.sendDefault(key, value).get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            log.error("Error Sending the message. Exception is: {}", e.getMessage());
+            throw e;
+        }
+
+        return sendResult;
     }
 
     private void handleFailure(Integer key, String value, Throwable throwable) {
