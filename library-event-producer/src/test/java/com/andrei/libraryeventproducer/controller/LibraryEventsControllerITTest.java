14e166e203158e0873f6c68a3b70fc77ec2d459c
Andrei Balzan <andreibalzan@gmail.com>
2021-09-27 14:28:16 +0100

    add integrated kafka to run integration tests

--- library-event-producer/src/test/java/com/andrei/libraryeventproducer/controller/LibraryEventsControllerITTest.java
+++ library-event-producer/src/test/java/com/andrei/libraryeventproducer/controller/LibraryEventsControllerITTest.java
@@ -8,10 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//property 'spring.embedded.kafka.brokers' come from Spring test library org.springframework.kafka.test.EmbeddedKafkaBroker
+//in the case we are just overwriting the properties from application.yml to run the tests.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"
+})
 class LibraryEventsControllerITTest {
 
     @Autowired
