98c010a8acba6faad76a9b785071e8acff10d737
Andrei Balzan <andreibalzan@gmail.com>
2021-09-29 16:02:59 +0100

    initial kafka consumer config

---
+++ library-event-consumer/src/main/java/andrei/libraryeventconsumer/config/LibraryEventConsumerConfig.java
@@ -0,0 +1,10 @@
+package andrei.libraryeventconsumer.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventConsumerConfig {
+}
